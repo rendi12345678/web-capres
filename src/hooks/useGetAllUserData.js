34bc6f020d1634ba1efb5ea2cf4d025023205545
rendi12345678 <hardleberg@gmail.com>
2024-02-19 12:14:15 +0800

    Added custom hooks

--- src/hooks/useGetAllUserData.js
+++ src/hooks/useGetAllUserData.js
@@ -5,17 +5,15 @@ function useGetAllUserData() {
   const { id, alasan, getData } = useContextHook();
   const [users, setUsers] = useState([]);
   const [isError, setIsError] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const setAllUsersData = async () => {
-      setIsLoading(true);
       try {
         const { users = [] } = await getData("/users");
 
         if (users.length === 0) return setIsError(true);
         setUsers(users);
-        setIsError(false);
       } finally {
         setIsLoading(false);
       }

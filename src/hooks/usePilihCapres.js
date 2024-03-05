b449867a3a2bf7d5ebf3e5fd640ba8ec91f8e60c
rendi12345678 <hardleberg@gmail.com>
2024-03-05 12:46:19 +0800

    Optimized performance and fixed bugs

--- src/hooks/usePilihCapres.js
+++ src/hooks/usePilihCapres.js
@@ -50,15 +50,20 @@ function usePilihCapres() {
       if (inputAlasan.length < 10)
         return alert("Input alasan minimmal 10 karakter!");
 
-      setCapresIdAction(id);
+      if (inputAlasan.length > 200)
+        return alert("Input alasan maksimal 200 karakter!");
+
       const data = await postData(`/pilih-capres/${cookies.token}/${id}`, {
         pilihanCapresId: id,
         alasan: inputAlasan,
       });
 
-      if (data.success) {
-        setAlasanAction(inputAlasan);
+      if (!data.success) {
+        return alert(data.msg);
       }
+
+      setCapresIdAction(id);
+      setAlasanAction(inputAlasan);
     },
     [capresId, isAuthorized, alasan]
   );
